76abb8440e97e6017fe108c54ef2a70a4a1cf3b1
Master <master@imac-de-lasallebcn-3.local>
2016-02-02 19:34:35 +0100

    Initial Commit

---
+++ PropertyCross/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PropertyCross
+//
+//  Created by Master on 2/2/16.
+//  Copyright © 2016 La Salle Master. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
